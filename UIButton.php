d7ce9f60eaa256144c28238291849ede3e66b90b
Max <61143434+FibonacciFox@users.noreply.github.com>
2021-03-31 17:38:39 +0700

    Add files via upload

---
+++ UIButton.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MarbleUI;
+
+class UIButton extends UIWidget
+{
+
+
+    protected function setPosition(array $position)
+    {
+        // TODO: Implement setPosition() method.
+    }
+
+    protected function setText(string $text)
+    {
+        // TODO: Implement setText() method.
+    }
+}
+
+
